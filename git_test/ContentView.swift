d407b78c39166ebc4f211dd1b3ec2349cc7baaab
summer67333 <summer67333@gmail.com>
2021-08-08 22:27:22 +0800

    Initial Commit

---
+++ git_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_test
+//
+//  Created by 陳皇成 on 2021/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
